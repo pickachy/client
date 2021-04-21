a5174b15e09207ca36ea83a732c4b878319537fd
Maksym <malsimushkka@gmail.com>
2021-04-21 21:47:22 +0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {PageNotFoundComponent} from "./common/pages/page-not-found.component";
+import {LoansInAdvancePageComponent} from "./pages/loans/in-advance/loans-in-advance-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'loans', children:
+      [
+        {path: 'in-advance', component: LoansInAdvancePageComponent}
+      ]
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabled',
 })],
   exports: [RouterModule]
 })
