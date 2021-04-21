57e359fc7b5d3819df8bf437d72d40322ee7618c
Maksym <malsimushkka@gmail.com>
2021-04-21 21:48:11 +0300

    remove redundant from package json | connect modules | refactor app html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CommonModule} from "./common/common.module";
+import {PagesModule} from "./pages/pages.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
