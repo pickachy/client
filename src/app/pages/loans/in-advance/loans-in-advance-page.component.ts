7efb1202266c6a7f71347630ef33ffd1d86230a5
Maksym <malsimushkka@gmail.com>
2021-04-21 21:47:02 +0300

    add pages

---
+++ src/app/pages/loans/in-advance/loans-in-advance-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-loans-in-advance',
+  templateUrl: './loans-in-advance-page.component.html',
+})
+export class LoansInAdvancePageComponent {
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import {HomePageComponent} from "./home-page/home-page.component";
+import {LoansInAdvancePageComponent} from "./loans/in-advance/loans-in-advance-page.component";
+
+@NgModule({
+  declarations: [HomePageComponent, LoansInAdvancePageComponent],
+  exports:[HomePageComponent, LoansInAdvancePageComponent],
+})
+export class PagesModule { }
