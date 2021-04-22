c9938a905a0523f09c0121c8d922489d8275e96d
Maksym <malsimushkka@gmail.com>
2021-04-22 21:57:33 +0300

    add code refactoring by prettier

--- src/app/pages/loans/in-advance/loans-in-advance-page.component.ts
+++ src/app/pages/loans/in-advance/loans-in-advance-page.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-loans-in-advance',
-  templateUrl: './loans-in-advance-page.component.html',
+  templateUrl: './loans-in-advance-page.component.html'
 })
-export class LoansInAdvancePageComponent {
-}
+export class LoansInAdvancePageComponent {}
