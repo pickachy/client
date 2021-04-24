c8189fbd99b01944fdfcbcf08ecb40137df55d7d
Maksym <malsimushkka@gmail.com>
2021-04-24 14:35:52 +0300

    add services section

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoansInAdvancePageComponent } from './loans/in-advance/loans-in-advance-page.component';
+import { ServicesComponent } from './home-page/sections/services/services.component';
+import { ServiceItemComponent } from './home-page/sections/services/service-item/service-item.component';
 
 @NgModule({
-  declarations: [HomePageComponent, LoansInAdvancePageComponent],
+  declarations: [HomePageComponent, ServicesComponent, ServiceItemComponent, LoansInAdvancePageComponent],
   exports: [HomePageComponent, LoansInAdvancePageComponent]
 })
 export class PagesModule {}
