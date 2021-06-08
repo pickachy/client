bb911a26debacdeb4b70b4b0ba5f00a29446f55e
Maksym <malsimushkka@gmail.com>
2021-06-09 00:19:23 +0300

    remove annotations | remove redundant

--- Api/ZemisApi.Core/Models/Loan.cs
+++ Api/ZemisApi.Core/Models/Loan.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using ZemisApi.Core.Models.Enums;
 
 namespace ZemisApi.Core.Models
@@ -15,17 +14,11 @@ namespace ZemisApi.Core.Models
         public bool IsOnlineProcessing { get; set; }
         public int BorrowerAgeFrom { get; set; }
         public int BorrowerAgeTo { get; set; }
-        [MaxLength(1500)]
         public string CommissionDescription { get; set; }
-        [MaxLength(200)]
         public string IssueWorkingHoursDescription { get; set; }
-        [MaxLength(1500)]
         public string RepaymentMethodsDescription { get; set; }
-        [MaxLength(1500)]
         public string WithdrawalMethodsDescription { get; set; }
-        [MaxLength(1500)]
         public string BonusesDescription { get; set; }
-        [MaxLength(1500)]
         public string ExtraInfo { get; set; }
     }
 }
