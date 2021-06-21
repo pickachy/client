f34c200ff510aa31e3bcf73604ecb1fd211d9351
Maksym <malsimushkka@gmail.com>
2021-06-21 19:48:19 +0300

    fix seeding script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,9 @@ done
 >&2 echo "Mysql is up - executing command"
 
 
+# deploying api service, so client will be able to execute prerendering
+docker-compose up -d api
+
 # waiting for seeded data
 until docker exec -it db mysql -h 127.0.0.1 -u "root" -c -proot --silent --execute="SELECT 1 FROM Loan"; do
   >&2 echo "Loans data has not been seeded yet... Waiting"
@@ -24,8 +27,6 @@ until docker exec -it db mysql -h 127.0.0.1 -u "root" -c -proot --silent --execu
   sleep 10
 done
 
-# deploying api service, so client will be able to execute prerendering
-docker-compose up -d api
 docker-compose up -d client
 docker-compose up -d proxy
 
