eda2c577f806aa634cf00ce04bebb237471c986b
Maksym <malsimushkka@gmail.com>
2021-06-21 19:15:00 +0300

    add not found page

--- Client/src/app/shared/pages/page-not-found.component.ts
+++ Client/src/app/shared/pages/page-not-found.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pages-not-found',
+  styleUrls: ['./page-not-found.component.scss'],
   templateUrl: './page-not-found.component.html'
 })
 export class PageNotFoundComponent {}
