1079002a385948733008ea0731843229990b9f78
Maksym <malsimushkka@gmail.com>
2021-06-22 21:15:24 +0300

    add http redirection | add certificate update

--- Proxy/entrypoint.sh
+++ Proxy/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 certbot certonly --standalone -d zemis.com.ua,www.zemis.com.ua --email malsimushkka@gmail.com -n --agree-tos --expand
+certbot renew --webroot --webroot-path /var/lib/certbot/ --post-hook "/usr/sbin/nginx -s reload"
 
 /usr/sbin/nginx -g "daemon off;"
