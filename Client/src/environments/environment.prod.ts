fcaa64d89fcd3a0a421dc9643f29d8594ca3a68a
Maksym <malsimushkka@gmail.com>
2021-06-27 02:44:07 +0300

    set http temporary

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  publicApiUrl: 'https://zemis.com.ua/api/v1/graphql',
+  publicApiUrl: 'http://zemis.com.ua/api/v1/graphql',
   serverApiUrl: 'http://api:5000/api/v1/graphql'
 };
