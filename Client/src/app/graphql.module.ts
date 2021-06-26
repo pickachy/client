d56146222581b43ac05b1e937341bb09025345a9
Maksym <malsimushkka@gmail.com>
2021-06-26 19:25:50 +0300

    fix ssr !

--- Client/src/app/graphql.module.ts
+++ Client/src/app/graphql.module.ts
@@ -1,25 +1,43 @@
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 import { environment } from '../environments/environment';
-import { isBrowser } from '@shared/tools/environmentUtils';
+import { makeStateKey, TransferState } from '@angular/platform-browser';
 
-export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+const APOLLO_CACHE = new InjectionToken<InMemoryCache>('apollo-cache');
+const STATE_KEY = makeStateKey<any>('apollo.state');
+
+export function createApollo(httpLink: HttpLink, cache: InMemoryCache, transferState: TransferState): ApolloClientOptions<any> {
+  const isBrowser = transferState.hasKey<any>(STATE_KEY);
+
+  if (isBrowser) {
+    const state = transferState.get<any>(STATE_KEY, null);
+    cache.restore(state);
+  } else {
+    transferState.onSerialize(STATE_KEY, () => {
+      return cache.extract();
+    });
+  }
   return {
+    ssrMode: true,
     link: httpLink.create({
-      uri: isBrowser ? environment.publicApiUrl : environment.serverApiUrl
+      uri: environment.apiUrl
     }),
-    cache: new InMemoryCache()
+    cache
   };
 }
 
 @NgModule({
   providers: [
     {
+      provide: APOLLO_CACHE,
+      useValue: new InMemoryCache()
+    },
+    {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink]
+      deps: [HttpLink, APOLLO_CACHE, TransferState]
     }
   ]
 })
