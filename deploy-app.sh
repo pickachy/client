6478abdae69c0dc76e6d0892a5b3e04bb9acfaaf
Maksym <malsimushkka@gmail.com>
2021-06-27 13:36:04 +0300

    add full rebuilding on deployment scripts

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # deploying api service, so client will be able to execute prerendering
-docker-compose up -d --build --no-deps api
+docker-compose build api
+docker-compose up --no-deps -d api
 
-docker-compose up -d --build --no-deps client
+
+docker-compose build client
+docker-compose up --no-deps -d client
