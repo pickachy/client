7114eae16f1adcfed2501dcaf29d1c3d2102a2cb
Maksym <malsimushkka@gmail.com>
2021-07-14 19:42:04 +0300

    remove unused modules

--- Client/src/app/pages/pages.module.ts
+++ Client/src/app/pages/pages.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LoansInAdvancePageComponent } from './loans/in-advance/loans-in-advance-page.component';
-import { SharedModule } from '@shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoansListComponent } from './loans/in-advance/sections/loans-list/loans-list.component';
 import { LoanInAdvanceSinglePageComponent } from './loans/in-advance/loan-single/loan-in-advance-single-page.component';
-import { RouterModule } from '@angular/router';
 import { CalculatorComponent } from './loans/in-advance/loan-single/sections/calculator/calculator.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,7 @@ import { CalculatorComponent } from './loans/in-advance/loan-single/sections/cal
     CalculatorComponent
   ],
   exports: [LoansInAdvancePageComponent, LoanInAdvanceSinglePageComponent],
-  imports: [SharedModule, CommonModule, FormsModule, FormsModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, AppRoutingModule, ReactiveFormsModule],
   providers: []
 })
 export class PagesModule {}
