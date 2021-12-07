ea18dbd27cfb76977d465e668336ff89b0c73e14
Nikolaienko Ihor <nikolasha8876@gmail.com>
2021-12-07 23:58:46 +0200

    added responcive

---
+++ src/app/pages/single-news/navigation-through-articles/articles-nav.component.ts
@@ -0,0 +1,10 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-page-single-news',
+//   templateUrl: './articles-nav.component.html',
+//   styleUrls: ['./articles-nav.component.scss']
+// })
+// export class ArticlesNavigationComponent {
+
+// }
