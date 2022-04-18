87109e92f13dc797c5e4f114be1a23fc011a3df0
Maksym <malsimushkka@gmail.com>
2022-04-18 15:20:52 +0300

    disable https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  host: 'https://zemis.com.ua',
-  publicApiUrl: 'https://zemis.com.ua/api/v1/graphql',
+  host: 'http://zemis.com.ua',
+  publicApiUrl: 'http://zemis.com.ua/api/v1/graphql',
   serverApiUrl: 'http://api:5000/api/v1/graphql'
 };
